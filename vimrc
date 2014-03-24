2d6ebac795fd7112b9c121416972d3ff473547a5
Jonathan Starr <jonstarr@utexas.edu>
2014-03-23 21:16:09 -0500

    Support xscreensaver in xinitrc and xmonad.hs.

--- vimrc
+++ vimrc
@@ -30,3 +30,13 @@ set guioptions+=LlrRb
 set guioptions-=LlrRb
 set guioptions-=m  "remove menu bar
 set guioptions-=T  "remove toolbar
+
+" grep will sometimes skip displaying the file name if you search in a singe
+" file. This will confuse Latex-Suite. Set your grep program to always
+" generate a file-name.
+set grepprg=grep\ -nH\ $*
+
+" Starting with Vim 7, the filetype of empty .tex files defaults to 'plaintex'
+" instead of 'tex', which results in vim-latex not being loaded.
+" The following changes the default filetype back to 'tex':
+let g:tex_flavor='latex'
--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,26 @@
 import XMonad
+import XMonad.Util.EZConfig
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.DynamicLog
+import System.IO
 
-main = xmonad =<< xmobar defaultConfig
-     { terminal ="urxvt -e /bin/zsh"
-     , modMask = mod4Mask
-     }
+-- The main function.
+main = xmonad =<< statusBar myBar myPP toggleStrutsKey myConfig
+
+-- Command to launch the bar.
+myBar = "xmobar"
+
+-- Custom PP, configure it as you like. It determines what is being written to the bar.
+myPP = xmobarPP { ppCurrent = xmobarColor "#429942" "" . wrap "<" ">" }
+
+-- Key binding to toggle the gap for the bar.
+toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+
+-- Main configuration, override the defaults to your liking.
+myConfig = defaultConfig 
+         { terminal = "urxvt -e /bin/zsh"
+         , modMask = mod4Mask
+         }
+         `additionalKeysP`
+         [ ("M-x", spawn "xscreensaver-command -lock")
+         ]
