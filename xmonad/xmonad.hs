16498699dd2a31ebf070974209062fac7e6ae4dc
Jonathan Starr <jonstarr@utexas.edu>
2015-10-14 11:43:10 -0500

    Various changes to xmonad setup.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,13 @@
 import XMonad
+
+import qualified Data.Map as M
+import Graphics.X11.Xlib
 import XMonad.Util.EZConfig
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.SetWMName
 import XMonad.Hooks.DynamicLog
+import XMonad.Actions.PhysicalScreens
+
 import System.IO
 
 -- The main function.
@@ -18,9 +24,18 @@ toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 -- Main configuration, override the defaults to your liking.
 myConfig = defaultConfig 
-         { terminal = "urxvt -e /bin/zsh"
-         , modMask = mod4Mask
+         { terminal = "st"
+         , startupHook = setWMName "LG3D"
+         , keys = myKeys <+> keys defaultConfig
          }
          `additionalKeysP`
          [ ("M-x", spawn "xscreensaver-command -lock")
          ]
+
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList
+         -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
+         -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
+         --
+         [((modm .|. mask, key), f sc)
+             | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+             , (f, mask) <- [(viewScreen, 0), (sendToScreen, shiftMask)]]
