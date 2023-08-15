8064223ab7bdfaf33d6c38d96ea23a4eacbb49e4
Jonathan Starr <github@jstarr.me>
2023-08-15 11:56:17 -0500

    Add rainbow delimiting for treesitter

--- config/nvim/lua/core/plugins.lua
+++ config/nvim/lua/core/plugins.lua
@@ -66,6 +66,7 @@ return packer.startup(function(use)
 
     --> context highlighting
     use 'nvim-treesitter/nvim-treesitter'
+    use 'HiPhish/rainbow-delimiters.nvim'
 
     --> lsp, linters, dignostics
     use {
