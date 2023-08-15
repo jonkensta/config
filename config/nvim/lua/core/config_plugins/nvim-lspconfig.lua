b9afadd933cba7955b1e8fb67d8bde0e5ffbf33d
Jonathan Starr <github@jstarr.me>
2023-08-15 13:15:30 -0500

    Add LSP keymaps to try out

--- config/nvim/lua/core/config_plugins/nvim-lspconfig.lua
+++ config/nvim/lua/core/config_plugins/nvim-lspconfig.lua
@@ -1,10 +1,20 @@
 local on_attach = function(client, bufnr)
-    --> keymaps
-    --vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-    --vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {}) --> close split `:on` (only)
-    --vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-    --vim.keymap.set("n", "F", vim.lsp.buf.format, {})
-    --vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+    local opts = { buffer = bufnr }
+    vim.keymap.set('n', '<leader>gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>gi', vim.lsp.buf.implementation, opts)
+
+    vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<leader>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+
+    vim.keymap.set('n', '<leader>K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader><C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
 
     vim.api.nvim_create_user_command("LspRename", "lua vim.lsp.buf.rename()", {})
 
