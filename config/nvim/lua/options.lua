3e35bf2260bb3e94f0849e1138805c1c39dd1caa
Jonathan Starr <github@jstarr.me>
2024-02-07 14:19:06 -0600

    Reformat `lua` comment spacing

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,8 +1,8 @@
 -- General settings
-vim.opt.expandtab = true        -- Converts tabs to whitespace
-vim.opt.tabstop = 4             -- Number of columns occupied by a tab character
-vim.opt.shiftwidth = 4          -- Width for autoindents
-vim.opt.softtabstop = 4         -- See multiple spaces as tabstops so <BS> does the right thing
-vim.opt.background = 'dark'     -- Set background to dark
+vim.opt.expandtab = true    -- Converts tabs to whitespace
+vim.opt.tabstop = 4         -- Number of columns occupied by a tab character
+vim.opt.shiftwidth = 4      -- Width for autoindents
+vim.opt.softtabstop = 4     -- See multiple spaces as tabstops so <BS> does the right thing
+vim.opt.background = 'dark' -- Set background to dark
 vim.opt.completeopt = "menuone,noselect"
 vim.opt.exrc = true
